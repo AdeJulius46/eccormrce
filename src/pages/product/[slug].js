4aa46e3386ef42cab062711e999c1a9be7b3c0c5
julius <adegokejulius46@gmail.com>
2023-02-28 10:33:36 +0100

    ah

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -27,6 +27,7 @@ const ProductDetails = ({product,products}) => {
             >
                  
                 {image?.map((item,i) =>(<img
+                  key={i}
                  src={urlFor(item)}
                  className= {i === index ? 'small-image selected-image':'small-image' }
                  onMouseEnter={()=> setIndex(i)}
